0369b00f3d4d5bab6a70f10ea613834e61af332e
Carl Kakisis <Messinias1@gmail.com>
2019-12-14 00:11:30 -0500

    c sizeof

---
+++ cMasterClass/sizeOf.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+
+int main()
+{
+    printf("%zd\n", sizeof(int));
+    printf("%zd\n", sizeof(char));
+    printf("%zd\n", sizeof(long));
+    printf("%zd\n", sizeof(long long));
+    printf("%zd\n", sizeof(double));
+    printf("%zd\n", sizeof(long double));
+    return 0;
+}
